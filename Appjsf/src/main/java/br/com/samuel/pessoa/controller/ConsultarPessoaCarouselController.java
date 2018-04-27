38c0a344ed9b82cb277701234ede709e6d3c1ea3
Samuel Batalha <samuelwsb@gmail.com>
2018-04-27 10:56:11 -0300

    Add files via upload

---
+++ Appjsf/src/main/java/br/com/samuel/pessoa/controller/ConsultarPessoaCarouselController.java
@@ -0,0 +1,40 @@
+package br.com.samuel.pessoa.controller;
+ 
+import java.io.Serializable;
+import java.util.List;
+ 
+import javax.annotation.PostConstruct;
+import javax.enterprise.inject.Produces;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+ 
+import br.com.samuel.model.PessoaModel;
+import br.com.samuel.repository.PessoaRepository;
+ 
+@Named(value="consultarPessoaCarouselController")
+@ViewScoped
+public class ConsultarPessoaCarouselController implements Serializable {
+ 
+	private static final long serialVersionUID = 1L;
+ 
+	@Inject transient
+	private PessoaRepository pessoaRepository;
+ 
+	@Produces 
+	private List<PessoaModel> pessoas;
+ 
+	public List<PessoaModel> getPessoas() {
+		return pessoas;
+	}
+ 
+	@PostConstruct
+	private void init(){
+ 
+		this.pessoas = pessoaRepository.GetPessoas();
+	}
+ 
+ 
+ 
+ 
+}
